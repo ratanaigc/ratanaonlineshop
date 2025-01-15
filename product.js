41c553e18dd50c8818e52c0f8b38305b005e91d2
ratanaigc <keokhouch1@gmail.com>
2025-01-15 22:19:43 +0700

    Create product.js

---
+++ product.js
@@ -0,0 +1,14 @@
+const product = [
+    { 
+        model: "camel 12l", 
+        category: "គឿងសម្ភារៈក្នុងផ្ទះ", 
+        name: "ឆ្នាំងអគ្គិសនី មិនបាច់ប្រើខ្លាញ់",
+        price: "46$",
+        photo: "./img/product/camel.png",
+        detail: "...",
+        address: "Apple Store",
+        phone: "123-456-7890",
+    },
+
+    // អ្នកអាចបន្ថែមទិន្នន័យផ្សេងៗនៅទីនេះ
+];
